4b148daf993a828f6d82ec8a95797063d9b24596
Spencer <spencer.toole@gmail.com>
2016-06-22 16:11:03 -0400

    first exercise changes

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,4 +4,10 @@ class Admin::UsersController < Admin::ApplicationController
 		@users = User.page(params[:page]).per(5)
 	end
 
+	def destroy
+  	@user = User.find(params[:id])
+  	@user.destroy
+  	redirect_to admin_users_path
+  end
+
 end
